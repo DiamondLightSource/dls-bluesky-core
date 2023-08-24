512edd28bf812b5b99c125334c294e27006ed1a2
Joseph Ware <joseph.ware@diamond.ac.uk>
2023-08-24 11:32:33 +0100

    Move plans and stubs from BlueAPI into repository structure
    - Enforce mypy on untyped functions
    - Add documentation to packages explaining the functionality and requirements of plans, stubs
    - Use __export__ to explicitly export "standalone" stubs
    - Add tests to check compliance of plans, stubs to requirements
    - Ensure loop for 3.10 Future

--- src/dls_bluesky_core/__main__.py
+++ src/dls_bluesky_core/__main__.py
@@ -1,14 +1,15 @@
 from argparse import ArgumentParser
+from typing import Optional, Sequence
 
 from . import __version__
 
 __all__ = ["main"]
 
 
-def main(args=None):
+def main(args: Optional[Sequence[str]] = None) -> None:
     parser = ArgumentParser()
     parser.add_argument("-v", "--version", action="version", version=__version__)
-    args = parser.parse_args(args)
+    parser.parse_args(args)
 
 
 # test with: python -m dls_bluesky_core
--- src/dls_bluesky_core/plans/scanspec.py
+++ src/dls_bluesky_core/plans/scanspec.py
@@ -1,13 +1,19 @@
 import operator
 from functools import reduce
-from typing import Any, List, Mapping, Optional, Union
+from typing import Any, List, Mapping, Optional
 
 import bluesky.plans as bp
 from bluesky.protocols import Movable, Readable
 from cycler import Cycler, cycler
 from scanspec.specs import Spec
 
-from blueapi.core import MsgGenerator
+from dls_bluesky_core.core import MsgGenerator
+
+"""
+Plans related to the use of the `ScanSpec https://github.com/dls-controls/scanspec`
+library for constructing arbitrarily complex N-dimensional trajectories, similar to
+Diamond's "mapping scans" using ScanPointGenerator.
+"""
 
 
 def scan(
@@ -71,43 +77,3 @@ def _scanspec_to_cycler(spec: Spec[str], axes: Mapping[str, Movable]) -> Cycler:
     # Need to "add" the cyclers for all the axes together. The code below is
     # effectively: cycler(motor1, [...]) + cycler(motor2, [...]) + ...
     return reduce(operator.add, map(lambda args: cycler(*args), midpoints.items()))
-
-
-def count(
-    detectors: List[Readable],
-    num: int = 1,
-    delay: Optional[Union[float, List[float]]] = None,
-    metadata: Optional[Mapping[str, Any]] = None,
-) -> MsgGenerator:
-    """
-    Take `n` readings from a device
-
-    Args:
-        detectors (List[Readable]): Readable devices to read
-        num (int, optional): Number of readings to take. Defaults to 1.
-        delay (Optional[Union[float, List[float]]], optional): Delay between readings.
-                                                               Defaults to None.
-        metadata (Optional[Mapping[str, Any]], optional): Key-value metadata to include
-                                                          in exported data.
-                                                          Defaults to None.
-
-    Returns:
-        MsgGenerator: _description_
-
-    Yields:
-        Iterator[MsgGenerator]: _description_
-    """
-    plan_args = (
-        {  # If bp.count added delay to plan_args, we could remove all md handling
-            "detectors": list(map(repr, detectors)),
-            "num": num,
-            "delay": delay,
-        }
-    )
-
-    _md = {
-        "plan_args": plan_args,
-        **(metadata or {}),
-    }
-
-    yield from bp.count(detectors, num, delay=delay, md=_md)
