ced4b8f0bd0bb6a65c6a88f7d0e2952084135df8
Ware, Joseph (DLSLtd,RAL,LSCI) <joseph.ware@diamond.ac.uk>
2023-10-13 15:19:41 +0100

    Move i22 generic plans (but do not expose to BlueAPI)

---
+++ src/dls_bluesky_core/stubs/flyables.py
@@ -0,0 +1,23 @@
+import bluesky.plan_stubs as bps
+from bluesky.protocols import Flyable
+
+from dls_bluesky_core.core import MsgGenerator, group_uuid
+
+
+def fly_and_collect(
+    flyer: Flyable, flush_period: float = 0.5, checkpoint_every_collect: bool = False
+) -> MsgGenerator:
+    yield from bps.kickoff(flyer)
+    complete_group = group_uuid("complete")
+    yield from bps.complete(flyer, group=complete_group)
+    done = False
+    while not done:
+        try:
+            yield from bps.wait(group=complete_group, timeout=flush_period)
+        except TimeoutError:
+            pass
+        else:
+            done = True
+        yield from bps.collect(flyer, stream=True, return_payload=False)
+        if checkpoint_every_collect:
+            yield from bps.checkpoint()
