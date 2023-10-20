efba33ee01cac60dd13c578d9b6ca92baa8b1bbd
Ware, Joseph (DLSLtd,RAL,LSCI) <joseph.ware@diamond.ac.uk>
2023-10-20 15:41:34 +0100

    Move inject method for type checked default arguments from blueapi

--- src/dls_bluesky_core/core/coordination.py
+++ src/dls_bluesky_core/core/coordination.py
@@ -12,3 +12,21 @@ def group_uuid(name: str) -> str:
         readable_uid (str): name appended with a unique string
     """
     return f"{name}-{str(uuid.uuid4())[:6]}"
+
+
+def inject(name: str):
+    """
+    Function to mark a default argument of a plan method as a reference to a device
+    that is stored in the Blueapi context.
+    Bypasses mypy linting, returning x as Any and therefore valid as a default
+    argument.
+
+    Args:
+        name (str): Name of a device to be fetched from the Blueapi context
+
+    Returns:
+        Any: name but without typing checking, valid as any default type
+
+    """
+
+    return name
