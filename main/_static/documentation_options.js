bd99805bf949d3fc732e5e1e9b8ecedfd0d1ebbc
rosesyrett <rosesyrett@users.noreply.github.com>
2023-11-14 16:33:09 +0000

    deploy: 459634f4d4e89c99aeec22249901526a659ea12d

--- main/_static/documentation_options.js
+++ main/_static/documentation_options.js
@@ -1,5 +1,5 @@
 const DOCUMENTATION_OPTIONS = {
-    VERSION: '0.0.2.dev1+gfc6625a',
+    VERSION: '0.0.2a1.dev2+g459634f',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
@@ -7,7 +7,7 @@ const DOCUMENTATION_OPTIONS = {
     LINK_SUFFIX: '.html',
     HAS_SOURCE: true,
     SOURCELINK_SUFFIX: '.txt',
-    NAVIGATION_WITH_KEYS: true,
+    NAVIGATION_WITH_KEYS: false,
     SHOW_SEARCH_SUMMARY: true,
     ENABLE_SEARCH_SHORTCUTS: true,
 };
