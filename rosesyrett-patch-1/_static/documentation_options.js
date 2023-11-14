a24b4e2c02ea4f439e64ae58e6bf99137f9bd344
rosesyrett <rosesyrett@users.noreply.github.com>
2023-11-14 16:31:02 +0000

    deploy: 4a73ddd44cf1f0917d81e55a0901971bd19474f1

---
+++ rosesyrett-patch-1/_static/documentation_options.js
@@ -0,0 +1,13 @@
+const DOCUMENTATION_OPTIONS = {
+    VERSION: '0.0.2a1.dev3+g4a73ddd',
+    LANGUAGE: 'en',
+    COLLAPSE_INDEX: false,
+    BUILDER: 'html',
+    FILE_SUFFIX: '.html',
+    LINK_SUFFIX: '.html',
+    HAS_SOURCE: true,
+    SOURCELINK_SUFFIX: '.txt',
+    NAVIGATION_WITH_KEYS: false,
+    SHOW_SEARCH_SUMMARY: true,
+    ENABLE_SEARCH_SHORTCUTS: true,
+};
