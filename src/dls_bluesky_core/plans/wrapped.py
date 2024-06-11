8e2128f6620d6557ce2338d4673eae5962fccd58
Keith Ralphs <keith.ralphs@diamond.ac.uk>
2024-06-11 12:56:50 +0000

    linting

--- src/dls_bluesky_core/plans/wrapped.py
+++ src/dls_bluesky_core/plans/wrapped.py
@@ -3,10 +3,9 @@ from typing import Any, List, Mapping, Optional, Union
 import bluesky.plans as bp
 import bluesky.preprocessors as bpp
 from bluesky.protocols import Readable
+from dodal.plans.data_session_metadata import attach_data_session_metadata_decorator
 
 from dls_bluesky_core.core import MsgGenerator
-
-from dodal.plans.data_session_metadata import attach_data_session_metadata_decorator
 
 """
 Wrappers for Bluesky built-in plans with type hinting and renamed metadata
@@ -27,11 +26,12 @@ def count(
         detectors (List[Readable]): Readable devices to read
         num (int): Number of readings to take. Defaults to 1.
         delay (Optional[Union[float, List[float]]]): Delay between readings.
-                                                               Defaults to None.
-        baseline (Optional[List[Readable]]): List of devices to read at start and end of scan
+                                                        Defaults to None.
+        baseline (Optional[List[Readable]]): List of devices to read at start
+                                                        and end of scan
         metadata (Optional[Mapping[str, Any]]): Key-value metadata to include
-                                                          in exported data.
-                                                          Defaults to None.
+                                                        in exported data.
+                                                        Defaults to None.
 
     Returns:
         MsgGenerator: _description_
@@ -43,7 +43,7 @@ def count(
 
     @bpp.baseline_decorator(baseline)
     @attach_data_session_metadata_decorator(provider=None)
-    def inner_plan():
+    def inner_plan() -> MsgGenerator:
         yield from bp.count(detectors, num, delay=delay, md=metadata)
 
     yield from inner_plan()
